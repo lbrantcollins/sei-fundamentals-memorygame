9e794dc0418f9ec2ab378852074bbffd3885ac9b
lindabcollins <lindabcollins@gmail.com>
2019-06-02 15:19:12 -0500

    created game logic: Added card shuffle

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -46,8 +46,21 @@ function flipCard() {
 	}
 }
 
+// Code to shuffle an array
+Array.prototype.shuffle = function() {
+    var input = this;
+    for (var i = input.length-1; i >=0; i--) {
+        var randomIndex = Math.floor(Math.random()*(i+1)); 
+        var itemAtIndex = input[randomIndex]; 
+        input[randomIndex] = input[i]; 
+        input[i] = itemAtIndex;
+    }
+    return input;
+}
+
 // Lay out the display of (shuffled) cards with back of cards showing
 function createBoard() {
+	cards.shuffle();
 	for (var i = 0; i < cards.length; i++) {
 		var cardElement = document.createElement('img');
 		cardElement.setAttribute('src', 'images/back.png');
